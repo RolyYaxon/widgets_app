ebdc09194d367e4851daa30f5ac4d477fb96ac64
Rolando <rryaxonr@gmail..com>
2023-07-17 18:14:01 -0600

    Modificamos todo

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const colorList = [Colors.blue, Colors.amber, Colors.redAccent, Colors.green];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, "Selected color must be greater than 0"),
+        assert(selectedColor < colorList.length,
+            "Selected color must be lesser or equal than ${colorList.length-1}");
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: false
+        )
+      );
+}
